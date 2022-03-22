bf057d9cecee40eeb7d7d96e59d91c6aa3d93aca
Hafsah <emekomahafsah@gmail.com>
2022-03-22 17:43:29 +0100

    feat: added testimonial page image

--- src/components/pages/work/SelectedWorks.js
+++ src/components/pages/work/SelectedWorks.js
@@ -74,65 +74,30 @@ const SelectedWorks = () => {
 }
 
 const sectionStyle = ctl(`
-    xl:px-[100px]
-    px-[80px]
-    mt-[124px]
+   
 `)
 const headingStyle = ctl(`
-    text-[28px]
-    mb-10
-    font-[600]
-    text-gm-black
+   
 `)
 const workTextWrapStyle = ctl(`
-    work-text-wrap
-    text-gm-grey
-    border-b-[1px]
-    border-gm-border-grey
-    pt-6
-    cursor-pointer
-    group
-    relative
+  
 `)
 const numberStyle = ctl(`
-    text-[12px]
-    mb-4
-    font-[700]
+    
 `)
 const workTitleStyle = ctl(`
-    work-title
-    xl:text-[56px]
-    text-[40px]
-    font-[600]
-    pb-8
-    group-hover:hidden
+   
 `)
 const workTitleStyle2 = ctl(`
-    work-title2
-    xl:text-[56px]
-    text-[40px]
-    font-[600]
-    pb-8
-    hidden
-    group-hover:block
-    transition-all duration-[1.5s] ease
+  
 `)
 const imageTitleStyle = ctl(`
-    uppercase
-    mb-4
-    lg:text-[14px] text-[12px]
-    font-[550]
+   
 `)
 const workDetailsStyle = ctl(`
-    absolute 
-    z-10 
-    right-0 top-5
-    opacity-0
-    group-hover:opacity-100
-    transition duration-[1s] ease
+   
 `)
 const workImageStyle = ctl(`
-    w-[35vw] xl:w-auto
 `)
 
 export default SelectedWorks
